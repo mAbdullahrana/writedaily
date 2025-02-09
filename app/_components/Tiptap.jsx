d7d6ad34590a18d45193d1f4f83f2498a8027bf4
mmAbdullahrana <ar292211@gmail.com>
2025-02-09 12:55:03 +0500

    Added Magic Button

--- app/_components/Tiptap.jsx
+++ app/_components/Tiptap.jsx
@@ -12,8 +12,25 @@ import { Color } from "@tiptap/extension-color";
 import TextStyle from "@tiptap/extension-text-style";
 import FloatingTextMenuBar from "./FloatingTextMenuBar";
 import TextMenuBar from "./TextMenuBar";
+import ExportToPDF from "./ExportToPDF";
 
 const init = `
+      <h1 style="text-align:center">
+        Devs Just Want to Have Fun by Cyndi Lauper
+      </h1>
+      <p style="text-align:center">
+        I come home in the morning light<br>
+        My mother says, <mark>“When you gonna live your life right?”</mark><br>
+        Oh mother dear we’re not the fortunate ones<br>
+        And devs, they wanna have fun<br>
+        Oh devs just want to have fun</p>
+      <p style="text-align:center">
+        The phone rings in the middle of the night<br>
+        My father yells, "What you gonna do with your life?"<br>
+        Oh daddy dear, you know you’re still number one<br>
+        But <s>girls</s>devs, they wanna have fun<br>
+        Oh devs just want to have
+      </p>
     `;
 
 export default function TipTap() {
@@ -50,6 +67,7 @@ export default function TipTap() {
 
   return (
     <>
+      {/* <ExportToPDF editor={editor} /> */}
       <TextMenuBar editor={editor} />
       <FloatingTextMenuBar editor={editor} />
       <EditorContent editor={editor} />
