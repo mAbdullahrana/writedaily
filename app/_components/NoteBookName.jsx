ecd03e00ae2e8390aeed7eda44c1b4a4d4d6acff
mmAbdullahrana <ar292211@gmail.com>
2025-02-10 22:26:45 +0500

    Working on User Table

--- app/_components/NoteBookName.jsx
+++ app/_components/NoteBookName.jsx
@@ -1,9 +1,24 @@
+import { getAllEntrie, updateNoteBookTitle } from "@/lib/actions";
 import { Check } from "lucide-react";
 import { useState } from "react";
 
-function NoteBookName({entrie}) {
-  const [name, setName] = useState(entrie?.title);
+function NoteBookName({ entrie }) {
+  const [title, setTitle] = useState(entrie?.title || "Add Page Title");
   const [isToggle, setIsToggle] = useState(false);
+
+  function handleUpdateName(e) {
+    e.preventDefault();
+    updateNoteBookTitle(title);
+    // const data = await getAllEntrie();
+
+    // console.log(data);
+    if (title === "") setTitle("Add Page Title");
+    setIsToggle((toggle) => !toggle);
+  }
+
+  function handleSetName(e) {
+    setTitle(e.target.value);
+  }
   function handleToggle(e) {
     e.preventDefault();
     setIsToggle((toggle) => !toggle);
@@ -16,20 +31,20 @@ function NoteBookName({entrie}) {
           className="text-mediumDark text-lg font-semibold px-4 hover:text-white transition"
           onClick={handleToggle}
         >
-          {name}
+          {title}
         </button>
       ) : (
         <div className="flex gap-1">
           <input
             className="bg-[#000] text-sm text-white px-3 py-[0.25rem] rounded-[0.8rem] outline-none"
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={title}
+            onChange={handleSetName}
           />
           <button
             className="text-mediumDark hover:text-white  bg-[#000] hover:bg-lightgray transition px-2 rounded-[0.8rem] "
             type="button"
-            onClick={handleToggle}
+            onClick={handleUpdateName}
           >
             <Check />
           </button>
